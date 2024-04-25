f0d6f69bf48666c9be99b9d83b5280e361ae34fc
Brigeman <yurgen.brigeman@mail.ru>
2024-04-25 21:57:15 +0200

    font for navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
-import './App.css'; // Убедитесь, что App.css импортирован
+import './App.css'; 
 
 function App() {
   return (
