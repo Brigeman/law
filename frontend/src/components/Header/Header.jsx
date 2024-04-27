1ec46c811664175f48b5ca485bf458f3601f416d
Brigeman <yurgen.brigeman@mail.ru>
2024-04-27 16:58:32 +0200

    header button

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,6 +3,22 @@ import Navbar from '../Navbar/Navbar';
 import './Header.css';
 
 function Header() {
+  // Text to display
+  const buttonText = "Заказать звонок";
+
+  // Function to apply faulty-letter class to every third letter
+  const styledText = (text) => {
+    return (
+      <span className='glowing-txt'>
+        {text.split('').map((char, index) => (
+          (index + 1) % 3 === 0
+            ? <span className='faulty-letter' key={index}>{char}</span>
+            : char
+        ))}
+      </span>
+    );
+  };
+
   return (
     <header className="header">
       <div className="upper-header">
@@ -10,11 +26,13 @@ function Header() {
           <div className="phone">+7 000 00 00</div>
           <div className="email">holyshit@yahoo.com</div>
         </div>
+        <button className='glowing-btn'>
+          {styledText(buttonText)}
+        </button>
       </div>
       <div className="lower-header">
         <div className="logo">Holyshittech</div>
         <Navbar />
-        <button className="call-button">Заказать звонок</button>
       </div>
     </header>
   );
